d53e3264b6061a2828683411bb74c661dd2769f2
Pham Nhat Thien <phamnhatthien87@gamil.com>
2026-09-14 21:03:39 +0700

    nộp bài 2

--- ch02_ex2/display_results.php
+++ ch02_ex2/display_results.php
@@ -17,8 +17,11 @@
         $error_message = 'Interest rate must be a valid number.'; 
     } else if ( $interest_rate <= 0 ) {
         $error_message = 'Interest rate must be greater than zero.'; 
+    } else if ( $interest_rate > 15 ) {
+        $error_message = 'Interest rate must be less than or equal to 15.';
+    }
     // validate years
-    } else if ( $years === FALSE ) {
+     else if ( $years === FALSE ) {
         $error_message = 'Years must be a valid whole number.';
     } else if ( $years <= 0 ) {
         $error_message = 'Years must be greater than zero.';
@@ -66,6 +69,7 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
+        <p>This calculation was done on <?php echo date('m/d/Y'); ?>.</p>
     </main>
 </body>
 </html>
